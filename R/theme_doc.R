97f9f6caa5fa873a7b7ae1a56489c781069cf650
Rishav Ray <rishav.rray@gmail.com>
2023-03-11 11:37:35 +0100

    fixed install

--- R/theme_doc.R
+++ R/theme_doc.R
@@ -1,11 +1,13 @@
+library(ggthemes)
+library(ggplot2)
 #' This is a redesigned ggplot theme built for out of the box scientific publication
 #' Use this theme with your normal ggplot function
 #'
 #' @export
 theme_doc <- function (base_size = 14, base_family = "sans") 
 {
-    (theme_foundation(base_size = base_size, base_family = base_family) + 
-        theme(line = element_line(colour = "black"), rect = element_rect(fill = "white", 
+    (ggthemes::theme_foundation(base_size = base_size, base_family = base_family) + 
+        ggplot2::theme(line = element_line(colour = "black"), rect = element_rect(fill = "white", 
             linetype = 0, colour = NA), text = element_text(colour = "black", size = 17), 
             axis.title = element_text(size = 20), axis.text.x = element_text(angle = 0), axis.text = element_text(size = 17), 
             axis.ticks = element_blank(), 
@@ -13,4 +15,4 @@ theme_doc <- function (base_size = 14, base_family = "sans")
             legend.direction = "horizontal", legend.box = "vertical", 
             panel.grid = element_line(colour = NULL), axis.line = element_line(colour = "black"), panel.grid.minor = element_blank(), plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"), panel.grid.major = element_line(colour = NA),plot.margin = unit(c(1, 1, 1, 1), "lines"), strip.background = element_rect()))
 }
-theme_set(theme_doc())
+ggplot2::theme_set(theme_doc())
