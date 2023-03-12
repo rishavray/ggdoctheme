98aae0897de103d5ac67ac8339fdcf383aca9969
Rishav Ray <rishav.rray@gmail.com>
2023-03-12 12:33:47 +0100

    added grid switch

--- R/theme_doc.R
+++ R/theme_doc.R
@@ -1,18 +1,38 @@
-library(ggthemes)
-library(ggplot2)
-#' This is a redesigned ggplot theme built for out of the box scientific publication
+#library(ggthemes)
+#library(ggplot2)
+#' @title doc ggplot theme
+#' @description This is a redesigned ggplot theme built for out of the box scientific publication
 #' Use this theme with your normal ggplot function
+#' @param base_size Base font size.
+#' @param base_family Base font family.
+#' @param grid Boolean switch to turn major grid on or off
 #'
 #' @export
-theme_doc <- function (base_size = 14, base_family = "sans") 
+theme_doc <- function (base_size = 14, base_family = "sans", grid = FALSE) 
 {
-    (ggthemes::theme_foundation(base_size = base_size, base_family = base_family) + 
-        ggplot2::theme(line = element_line(colour = "black"), rect = element_rect(fill = "white", 
-            linetype = 0, colour = NA), text = element_text(colour = "black", size = 17), 
-            axis.title = element_text(size = 20), axis.text.x = element_text(angle = 0), axis.text = element_text(size = 17), 
+    ret = (ggthemes::theme_foundation(base_size = base_size, base_family = base_family) + 
+        ggplot2::theme(line = element_line(colour = "black"),
+            rect = element_rect(fill = "white", linetype = 0, colour = NA),
+            text = element_text(colour = "black", size = 17), 
+            axis.title = element_text(size = 20),
+            axis.text.x = element_text(angle = 0),
+            axis.text = element_text(size = 17), 
             axis.ticks = element_blank(), 
-            legend.background = element_rect(), legend.position = "bottom", 
-            legend.direction = "horizontal", legend.box = "vertical", 
-            panel.grid = element_line(colour = NULL), axis.line = element_line(colour = "black"), panel.grid.minor = element_blank(), plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"), panel.grid.major = element_line(colour = NA),plot.margin = unit(c(1, 1, 1, 1), "lines"), strip.background = element_rect()))
+            legend.background = element_rect(),
+            legend.position = "bottom", 
+            legend.direction = "horizontal",
+            legend.box = "vertical", 
+            panel.grid = element_line(colour = NULL),
+            axis.line = element_line(colour = "black"),
+            panel.grid.minor = element_blank(),
+            plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
+            plot.margin = unit(c(1, 1, 1, 1), "lines"),
+            strip.background = element_rect()))
+    if(grid){
+        ret = ret + theme(panel.grid.major = element_line(colour = "#D2D2D2"))
+    }
+    else{
+        ret = ret + theme(panel.grid.major = element_line(colour = NA))
+    }
+    ret      
 }
-ggplot2::theme_set(theme_doc())
--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # ggdoctheme ![plot](./figures/icon.png)
-This is a redesigned ggplot theme built for out of the box scientific publication
+This is a redesigned ggplot theme built to be used for out of the box scientific publication.
 Use this theme with your normal ggplot function.
 
-The package depends on ggplot2 and ggthemes. Installing the package using devtools.
+### Installation
+The package depends on ggplot2 and ggthemes. Install the package using devtools.
 
 ```
 devtools::install_github("rishavray/ggdoctheme")
 ```
-
-Run the code like this in R
+### Theme demo
+Run the code like this in R. 
 ```
 ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
 geom_point() +
 facet_wrap(~Species) +
-theme_doc()+
+theme_doc(grid = FALSE)+
 labs(x = "Length", y = "Width")
 ```
 This creates a plot like this
 ![plot](./figures/plot.png)
+
+### Customization
+The grid function can be switched on and off by setting the **grid** parameter TRUE or FALSE.
+More customization to be followed.
